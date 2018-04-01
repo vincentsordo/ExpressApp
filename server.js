70d28f6c39b5d7324cdc338fc2d03daf7c0bb791
vincent <vsordo@gmail.com>
2018-04-01 13:09:19 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// middleware to keep track of server logs
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFileSync('server.log', log + '\n');
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (false) {
+    res.render('maintenance.hbs', {
+      pageTitle: 'Maintenance page',
+    });
+  }
+  else {
+    next();
+  }
+});
+
+// middleware for static files
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('uppercase', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to this Express App'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.status(404);
+  res.send({
+    errorMessage: 'Bad boy'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
